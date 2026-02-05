ea7970ef63eb0fd96d78544e251fb3679d39668c
mlhutilidades <mlhutilidades@gmail.com>
2026-02-05 15:41:07 -0300

    fix: extend IncomingMessage for rawBody

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,7 +1,14 @@
 import 'express-serve-static-core';
+import 'http';
 
 declare module 'express-serve-static-core' {
   interface Request {
+    rawBody?: Buffer;
+  }
+}
+
+declare module 'http' {
+  interface IncomingMessage {
     rawBody?: Buffer;
   }
 }
